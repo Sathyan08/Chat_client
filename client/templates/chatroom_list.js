d63c889b7631d2553e974aefe0fb301db5076302
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-10 15:54:53 -0500

    created dummy info for chatroom listings, and templates for the listings.

---
+++ client/templates/chatroom_list.js
@@ -0,0 +1,15 @@
+var chatData = [
+  {
+    title: "Team Discovery Channel",
+    description: "Spring forth burly protector! Discuss the Simpsons"
+
+  },
+  {
+    title: "Macho Business Donkey Wrestling",
+    description: "Discuss the autobiography of Jimmy James from News Radio"
+  }
+];
+
+Template.chatroomList.helpers({
+  chatrooms: chatData
+});
