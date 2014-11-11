51e75f2a2f77806d5d9986884886f881ebbcc483
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-11 14:48:56 -0500

    fixed permissions for chatrooms to be more expressive and specific to this app.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,18 +1,22 @@
 if (Chatrooms.find().count() === 0){
   Chatrooms.insert({
     title: "Team Discovery Channel",
-    description: "Spring forth burly protector! Discuss the Simpsons"
+    description: "Spring forth burly protector! Discuss the Simpsons",
+    author: 'Sathyan.Mathai@gmail.com'
   });
   Chatrooms.insert({
     title: "Macho Business Donkey Wrestling",
-    description: "Discuss the autobiography of Jimmy James from News Radio"
+    description: "Discuss the autobiography of Jimmy James from News Radio",
+     author: 'Sathyan.Mathai@gmail.com'
   });
   Chatrooms.insert({
     title: "Chicken Dance",
-    description: "Learn how to act with Carl Weathers"
+    description: "Learn how to act with Carl Weathers",
+    author: 'Sathyan.Mathai@gmail.com'
   });
   Chatrooms.insert({
     title: "Hall of Shame",
-    description: "Pull yourself out of bed long enough to discuss U.S. Politics."
+    description: "Pull yourself out of bed long enough to discuss U.S. Politics.",
+    author: 'Sathyan.Mathai@gmail.com'
   });
 }
