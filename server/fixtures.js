b04e1b06535540d2320b33d5445fad1a20d83e3e
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-11-11 14:33:00 -0500

    limited users ability to edit their chatroom listings so that they can only edit the title and the description (and not the author)

--- server/fixtures.js
+++ server/fixtures.js
@@ -7,4 +7,12 @@ if (Chatrooms.find().count() === 0){
     title: "Macho Business Donkey Wrestling",
     description: "Discuss the autobiography of Jimmy James from News Radio"
   });
+  Chatrooms.insert({
+    title: "Chicken Dance",
+    description: "Learn how to act with Carl Weathers"
+  });
+  Chatrooms.insert({
+    title: "Hall of Shame",
+    description: "Pull yourself out of bed long enough to discuss U.S. Politics."
+  });
 }
